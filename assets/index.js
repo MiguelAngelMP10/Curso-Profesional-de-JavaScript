764095fd714c52fcab3cf98ca6ddfa26068e1981
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2020-02-10 23:32:39 -0600

    Clusers, Scope

---
+++ assets/index.js
@@ -0,0 +1,11 @@
+import MediaPlayer from './MediaPlayer.js';
+
+const video = document.querySelector("video");
+const button = document.querySelector("button");
+
+
+const player = new MediaPlayer({
+    el: video
+});
+
+button.onclick = () => player.togglePlay();
