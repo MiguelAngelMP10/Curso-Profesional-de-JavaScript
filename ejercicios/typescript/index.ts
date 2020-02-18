4ab7b1c99cb1bfa99f3782a6e72245941ffc2aca
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2020-02-17 22:27:54 -0600

    funciones  en Typescript

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -48,3 +48,26 @@ comodin = { type: "Wildcard" };
 
 // Object
 let someObject: object = { type: "Wildcard" };
+
+// Funciones
+function addDos(a: number, b: number): number {
+  return a + b;
+}
+
+const sumDos: number = addDos(4, 6);
+
+function createAdder(a: number): (number) => number {
+  return function(b: number) {
+    return b + a;
+  };
+}
+
+const addFour = createAdder(4);
+const fourPlus6 = addFour(6);
+
+function fullName(firstName: string, lastName: string = "Smith"): string {
+  return `${firstName} ${lastName}`;
+}
+
+const miguel = fullName("Agente");
+console.log(miguel);
