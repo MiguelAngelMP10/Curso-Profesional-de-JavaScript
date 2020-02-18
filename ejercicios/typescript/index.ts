e1c2de1c17a7d3ab171a16d11ea63d9a270eed44
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2020-02-17 22:04:31 -0600

    Algunos tipos de datos en Typescript

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -5,3 +5,46 @@ function add(a: number, b: number) {
 }
 
 const sum = add(2, 3);
+
+//Tipos de datos
+
+//Boolean
+let muted: boolean = true;
+muted = false;
+
+// Números
+let age = 6;
+let numerador: number = 42;
+let denominador: number = age;
+let resultado = numerador / denominador;
+
+// String
+let nombre: string = "Miguel Angel";
+let saludo = `Me llamo ${nombre}`;
+
+// Arreglos
+let people: string[] = [];
+people = ["Isabel", "Nicole", "Raul"];
+// people.push("9000")
+
+let peopleAndNumbers: Array<string | number> = [];
+peopleAndNumbers.push("Ricardo");
+peopleAndNumbers.push(9001);
+
+// Enum
+enum Color {
+  Rojo = "Rojo",
+  Verde = "Verde",
+  Azul = "Azul",
+  Amarillo = "Amarillo"
+}
+
+let colorFavorito: Color = Color.Amarillo;
+console.log(`Mi color favorito es ${colorFavorito}`);
+
+// Any
+let comodin: any = "Joker";
+comodin = { type: "Wildcard" };
+
+// Object
+let someObject: object = { type: "Wildcard" };
