e11cbf38b54289a68112a6aeca2fd3e6ea034b23
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2020-02-20 20:43:38 -0600

    Creamos plugin Ads

--- assets/index.ts
+++ assets/index.ts
@@ -2,13 +2,15 @@ import MediaPlayer from "./MediaPlayer";
 import AutoPlay from "./plugins/AutoPlay";
 import AutoPause from "./plugins/AutoPause";
 
+import Ads from "./plugins/Ads";
+
 const video = document.querySelector("video");
 const btnPlayPause: HTMLElement = document.querySelector("#btn-play-pause");
 const btnMuteUnmute: HTMLElement = document.querySelector("#btn-mute-unmute");
 
 const player = new MediaPlayer({
   el: video,
-  plugins: [new AutoPlay(), new AutoPause()]
+  plugins: [new AutoPlay(), new AutoPause(), new Ads()]
 });
 
 btnPlayPause.onclick = () => player.togglePlay();
